b1549e8f480f79ab6931e9764a2a043428449a73
JUSTICEMENSAH-ALX <portjusto1@gmail.com>
2022-12-12 02:24:23 +0000

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# This installs the package puppet-lint
-
-package { 'puppet-lint':
+# Using Puppet, install flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3',
 }
